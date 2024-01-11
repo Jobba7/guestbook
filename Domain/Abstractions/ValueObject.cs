be182365b345d0d4ef2aef6fff7f4b26641c4853
Administrator <admin@example.com>
2024-01-11 20:24:42 +0100

    Add ValueObject

---
+++ Domain/Abstractions/ValueObject.cs
@@ -0,0 +1,4 @@
+﻿namespace Guestbook.Domain.Abstractions;
+#pragma warning disable S2094 // Classes should not be empty
+public abstract record ValueObject;
+#pragma warning restore S2094 // Classes should not be empty
