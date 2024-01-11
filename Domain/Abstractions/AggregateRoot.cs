5f4025871da95ff8da64ae3f997f065730a123d3
Administrator <admin@example.com>
2024-01-11 20:26:51 +0100

    Fix AggregateRoot

--- Domain/Abstractions/AggregateRoot.cs
+++ Domain/Abstractions/AggregateRoot.cs
@@ -1,7 +1,8 @@
 ﻿namespace Guestbook.Domain.Abstractions;
-public abstract class AggregateRoot : Entity<Guid>
+public abstract class AggregateRoot<TId> : Entity<TId>
+  where TId : notnull
 {
-  protected AggregateRoot(Guid id) : base(id)
+  protected AggregateRoot(TId id) : base(id)
   {
   }
 }
