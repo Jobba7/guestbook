ac5d100105f6e27ab3305a2a0711c35f9e72c6a5
Administrator <admin@example.com>
2024-01-14 18:23:29 +0100

    Implement json web token authentication

--- Guestbook.Infrastructure/Services/Authentication/JwtTokenGenerator.cs
+++ Guestbook.Infrastructure/Services/Authentication/JwtTokenGenerator.cs
@@ -1,20 +1,31 @@
 ﻿using Guestbook.Application.Services.Authentication;
 using Microsoft.IdentityModel.JsonWebTokens;
-using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
+using System.Text;
 
 namespace Guestbook.Infrastructure.Services.Authentication;
 public class JwtTokenGenerator : IJwtTokenGenerator
 {
   public string GenerateToken(Guid userId, string username)
   {
-    var claims = new[]
+    var tokenHandler = new JsonWebTokenHandler();
+
+    var key = Encoding.ASCII.GetBytes("key");
+
+    var tokenDescriptor = new SecurityTokenDescriptor()
     {
-      new Claim(JwtRegisteredClaimNames.Sub,userId.ToString()),
-      new Claim(JwtRegisteredClaimNames.UniqueName,username),
-      new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+      Subject = new ClaimsIdentity(new[]
+      {
+         new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+         new Claim(JwtRegisteredClaimNames.Name, username),
+      }),
+      Expires = DateTime.UtcNow.AddDays(1),
+      SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256),
     };
 
-    var token = new JwtSecurityToken(claims: claims);
+    var token = tokenHandler.CreateToken(tokenDescriptor);
+
+    return token;
   }
 }
