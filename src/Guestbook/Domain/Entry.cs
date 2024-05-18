bd73b254b97bb3cf7eac47f778fe762aef660f3c
Johannes <johannes.pforr@outlook.de>
2024-05-18 15:32:06 +0200

    add guest and entry

---
+++ src/Guestbook/Domain/Entry.cs
@@ -0,0 +1,22 @@
+﻿namespace Guestbook.Domain;
+
+public class Entry
+{
+  public Guid Id { get; private init; }
+
+  public string Content { get; private set; }
+
+  public Guid Author { get; private init; }
+
+  private Entry(Guid id, string content, Guid author)
+  {
+    Id = id;
+    Content = content;
+    Author = author;
+  }
+
+  public static Entry Create(string content, Guid author)
+  {
+    return new Entry(Guid.NewGuid(), content, author);
+  }
+}
---
+++ src/Guestbook/Domain/Guest.cs
@@ -0,0 +1,24 @@
+﻿namespace Guestbook.Domain;
+
+public class Guest
+{
+  public Guid Id { get; private init; }
+
+  public string Name { get; private set; }
+
+  public IReadOnlyList<Guid> Entries => entries.AsReadOnly();
+
+  private readonly List<Guid> entries;
+
+  private Guest(Guid id, string name, List<Guid> entries)
+  {
+    Id = id;
+    Name = name;
+    this.entries = entries;
+  }
+
+  public static Guest Create(string name)
+  {
+    return new Guest(Guid.NewGuid(), name, new List<Guid>());
+  }
+}
